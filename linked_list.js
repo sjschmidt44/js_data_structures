b53b7168485d6e07c51892f647503d799956b81f
Scott Schmidt <sjschmidt44@gmail.com>
2016-04-03 15:28:38 -0700

    Remove IIFE from linked list - working on mocha tests

--- linked_list.js
+++ linked_list.js
@@ -1,116 +1,112 @@
-(function(module) {
-  function linkedList(iterable) {
-    this.current = null;
-    this.head = null;
-    this.length = null;
+function linkedList(iterable) {
+  this.current = null;
+  this.head = null;
+  this.length = null;
 
-    if (iterable) {
-      iterable.forEach(function(idx) {
-        this.insert(idx);
-      }, this)
-    }
+  if (iterable) {
+    iterable.forEach(function(idx) {
+      this.insert(idx);
+    }, this)
   }
+}
 
-  linkedList.prototype.size = function() {
-    try {
-      if (this.length === null) {
-        throw "IndexError: There are no nodes in the list.";
-      } else {
-        return 'The Linked List is ' + this.length;
-      }
-    } catch (e) {
-      return "There was an error: " + e;
+linkedList.prototype.size = function() {
+  try {
+    if (this.length === null) {
+      throw "IndexError: There are no nodes in the list.";
+    } else {
+      return 'The Linked List is ' + this.length;
     }
+  } catch (e) {
+    return "There was an error: " + e;
   }
+}
 
-  linkedList.prototype.insert = function(val) {
-    try {
-      if (typeof val === 'undefined') {
-        throw "ValueError: Please enter a value for the node.";
-      } else {
-        this.head = new Node(val, this.head);
-        this.length += 1;
-        return null;
-      }
-    } catch (e) {
-      return "There was an error: \n" + e;
+linkedList.prototype.insert = function(val) {
+  try {
+    if (typeof val === 'undefined') {
+      throw "ValueError: Please enter a value for the node.";
+    } else {
+      this.head = new Node(val, this.head);
+      this.length += 1;
+      return null;
     }
+  } catch (e) {
+    return "There was an error: \n" + e;
   }
+}
 
-  linkedList.prototype.remove = function(val) {
-    try {
-      if (typeof val === 'undefined') {
-        throw "ValueError: Please enter a value for the node.";
-      } else if (this.search(val) === false){
-        throw "ValueError: That node does not exist in the list.";
-      } else {
-        this.current = this.head;
-        if (this.head.val === val) {
-          this.head = this.current.next;
-          this.current = null;
-          this.length -= 1;
-          return this.current;
-        } else {
-          while (this.current.next) {
-            if (this.current.next.val === val) {
-              this.current.next = this.current.next.next;
-              this.length -= 1;
-              return null;
-            } else {
-              this.current = this.current.next;
-            }
-          }
-        }
-      }
-    } catch (e) {
-      return 'There was an error \n' + e;
-    }
-  }
-
-  linkedList.prototype.pop = function() {
-    try {
+linkedList.prototype.remove = function(val) {
+  try {
+    if (typeof val === 'undefined') {
+      throw "ValueError: Please enter a value for the node.";
+    } else if (this.search(val) === false){
+      throw "ValueError: That node does not exist in the list.";
+    } else {
       this.current = this.head;
-      this.head = this.head.next;
-      this.length -= 1;
-      return this.current.val;
-    } catch (e) {
-      return "There was an error: \n" + e;
-    }
-  }
-
-  linkedList.prototype.search = function(val) {
-    try {
-      if (typeof val === 'undefined') {
-        throw "ValueError: Please enter a value for the node.";
+      if (this.head.val === val) {
+        this.head = this.current.next;
+        this.current = null;
+        this.length -= 1;
+        return this.current;
       } else {
-        this.current = this.head;
         while (this.current.next) {
-          if (this.current.val === val) {
-            return this.current;
+          if (this.current.next.val === val) {
+            this.current.next = this.current.next.next;
+            this.length -= 1;
+            return null;
           } else {
             this.current = this.current.next;
           }
         }
+      }
+    }
+  } catch (e) {
+    return 'There was an error \n' + e;
+  }
+}
+
+linkedList.prototype.pop = function() {
+  try {
+    this.current = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return this.current.val;
+  } catch (e) {
+    return "There was an error: \n" + e;
+  }
+}
+
+linkedList.prototype.search = function(val) {
+  try {
+    if (typeof val === 'undefined') {
+      throw "ValueError: Please enter a value for the node.";
+    } else {
+      this.current = this.head;
+      while (this.current.next) {
         if (this.current.val === val) {
           return this.current;
         } else {
-          return false;
+          this.current = this.current.next;
         }
       }
-    } catch (e) {
-      return "There was an error: \n" + e;
+      if (this.current.val === val) {
+        return this.current;
+      } else {
+        return false;
+      }
     }
+  } catch (e) {
+    return "There was an error: \n" + e;
   }
+}
 
-  linkedList.prototype.display = function() {
-    var nodes = [];
-    this.current = this.head;
-    for (var i = this.length; i > 0; i--) {
-      nodes.push(this.current.val);
-      this.current = this.current.next;
-    }
-    return nodes;
+linkedList.prototype.display = function() {
+  var nodes = [];
+  this.current = this.head;
+  for (var i = this.length; i > 0; i--) {
+    nodes.push(this.current.val);
+    this.current = this.current.next;
   }
-
-  module.linkedList = linkedList;
-})(window)
+  return nodes;
+}
