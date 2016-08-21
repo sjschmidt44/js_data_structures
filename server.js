14ddae6992be33c334e1ef081c14f478731808ff
Scott Schmidt <sjschmidt44@gmail.com>
2016-08-21 14:15:32 -0700

    remove server.js file; not necessary

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const express = require('express');
-const dataStructures = require('./lib/');
-const app = express();
-const port = process.argv[2] || 3000;
-
-app.listen(port, () => console.log('Listening on port', port));
