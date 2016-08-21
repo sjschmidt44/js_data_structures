bac75e8ef9ab0d8c31c481244e1b8952f6c17a38
Scott Schmidt <sjschmidt44@gmail.com>
2016-08-21 15:54:06 -0700

    complete stack methods, and minor test tweaks.

--- test/test_stack.js
+++ test/test_stack.js
@@ -6,14 +6,16 @@ const expect = require('chai').expect;
 describe('Stack()', () => {
   let stack;
   describe('new Stack()', () => {
-    stack = new Stack();
     it('should instantiate a new Stack object', () => {
+      stack = new Stack();
       expect(stack).to.be.instanceof(Stack);
     });
-    it('should have a null head value', () => {
+    it('should have a null top value', () => {
+      stack = new Stack();
       expect(stack.top).to.equal(null);
     });
     it('should have a size of 0', () => {
+      stack = new Stack();
       expect(stack.size).to.equal(0);
     });
   });
@@ -26,44 +28,48 @@ describe('Stack()', () => {
   });
 
   describe('new Stack() with array arg', () => {
-    stack = new Stack([2, 23, 32, 43, 50]);
     it('should have a size of 5', () => {
+      stack = new Stack([2, 23, 32, 43, 50]);
       expect(stack.size).to.equal(5);
     });
     it('should have a top val of 50', () => {
+      stack = new Stack([2, 23, 32, 43, 50]);
       expect(stack.top.val).to.equal(50);
     });
   });
 
   describe('push()', () => {
-    stack = new Stack();
     it('should add new val to top of stack', () => {
+      stack = new Stack();
       stack.push(3);
       expect(stack.top.val).to.equal(3);
     });
     it('should increase size of stack', () => {
+      stack = new Stack();
       let stackSize = stack.size;
       stack.push(54);
       expect(stackSize).to.equal(stack.size - 1);
     });
     it('should throw error when max size exceeded', () => {
-      stack = new Stack([4, 3, 2, 1]);
-      expect(stack.push(5)).to.be.isntanceof(TypeError);
+      stack = new Stack([4, 3, 2, 1], 4);
+
+      expect(stack.push(5)).to.be.instanceof(RangeError);
     });
     it('should throw error if no arg', () => {
+      stack = new Stack();
       expect(stack.push()).to.be.instanceof(ReferenceError);
     });
   });
 
   describe('pop()', () => {
-    stack = new Stack([43, 12]);
     it('should remove and return top val of stack', () => {
+      stack = new Stack([43, 12]);
       expect(stack.pop()).to.equal(12);
       expect(stack.top.val).to.equal(43);
     });
     it('should throw error if stack is empty', () => {
-      stack.pop();
-      expect(stack.pop()).to.be.instanceof(TypeError);
+      stack = new Stack();
+      expect(stack.pop()).to.be.instanceof(ReferenceError);
     });
   });
 
@@ -75,7 +81,7 @@ describe('Stack()', () => {
     });
     it('should throw error if stack is empty', () => {
       stack = new Stack();
-      expect(stack.peek()).to.be.isntanceof(TypeError);
+      expect(stack.peek()).to.be.instanceof(ReferenceError);
     });
   });
 });
