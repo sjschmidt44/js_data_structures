6d06777f45afd1da132d7732ab56c88e32641675
Scott Schmidt <sjschmidt44@gmail.com>
2016-08-21 16:04:41 -0700

    Scaffold queue and test-queue

---
+++ test/test_queue.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Queue = require('../lib/queue.js');
+const expect = require('chai').expect;
+
+describe('Queue()', () => {
+  describe('new Queue()', () => {
+
+  });
+
+  describe('enqueue()', () => {
+
+  });
+
+  describe('dequeue()', () => {
+
+  });
+});
