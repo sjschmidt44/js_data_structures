99cb3faee7d337a780d4a4c3f70efe5959e84544
Scott Schmidt <sjschmidt44@gmail.com>
2016-08-27 14:34:27 -0700

    Update insert method and test coverage

--- test/test_linked_list_dbl.js
+++ test/test_linked_list_dbl.js
@@ -18,22 +18,30 @@ describe('DoubleLinked()', () => {
     }));
   });
   describe('new DoubleLinked list with array arg, [12, 32, 43, 2]', () => {
-    it('should instantiate a new DoubleLinked list object', () => {
-
+    it('should return a value of 2 at head of list', () => {
+      dl = new DoubleLinked([12, 32, 43, 2]);
+      expect(dl.head.val).to.equal(2);
     });
-    it('should return a value of 12 at front of list', () => {
-
-    });
-    it('should return a value of 2 at tail of list', (() => {
-
+    it('should return a value of 12 at tail of list', (() => {
+      dl = new DoubleLinked([12, 32, 43, 2]);
+      expect(dl.tail.val).to.equal(12);
     }));
   });
   describe('insert()', () => {
     it('should insert a new value at the head of the list', () => {
-
+      dl = new DoubleLinked();
+      dl.insert(2);
+      expect(dl.head.val).to.equal(2);
+      expect(dl.tail.val).to.equal(2);
+      dl.insert(1);
+      expect(dl.head.val).to.equal(1);
+      expect(dl.tail.val).to.equal(2);
+      dl.insert(4);
+      expect(dl.head.val).to.equal(4);
+      expect(dl.tail.val).to.equal(2);
     });
     it('should throw error if no arg', () => {
-
+      expect(dl.insert()).to.be.instanceof(ReferenceError);
     });
   });
   describe('append()', () => {
