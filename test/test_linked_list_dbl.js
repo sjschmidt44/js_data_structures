a0dc3fc2ee6cae243e50627738b263f000dbe098
Scott Schmidt <sjschmidt44@gmail.com>
2016-08-27 09:16:13 -0700

    Scaffold double linked list and tests

---
+++ test/test_linked_list_dbl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const DoubleLinked = require('../lib/linked_list_dbl');
+const expect = require('chai');
+
+describe('DoubleLinked()', () => {
+  describe('new DoubleLinked list', () => {
+
+  });
+  describe('insert()', () => {
+
+  });
+  describe('append()', () => {
+
+  });
+  describe('pop()', () => {
+
+  });
+  describe('shift', () => {
+
+  });
+  describe('remove()', () => {
+
+  }); 
+});
